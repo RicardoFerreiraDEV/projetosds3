175f2b403c4e742c9470d197d9d702b9ca79b10b
Ricardo <ricardo.ferreira.ciss@gmail.com>
2021-05-05 19:40:47 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -16,7 +16,7 @@ public class Seller {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
+	private Long id;
 	private String name;
 	
 	@OneToMany(mappedBy = "seller")
@@ -26,16 +26,16 @@ public class Seller {
 
 	}
 
-	public Seller(String id, String name) {
+	public Seller(Long id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
