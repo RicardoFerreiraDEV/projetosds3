5b5d938880a9407d0c7c5a12b1c6f08b5799f7ba
Ricardo <ricardo.ferreira.ciss@gmail.com>
2021-05-19 12:30:28 -0300

    application 'dev'

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -1,14 +1,16 @@
 package com.devsuperior.dsvendas;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsvendasApplication {
+public class DsvendasApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsvendasApplication.class, args);
-		System.out.println("Ok!");
+		System.out.println("DsVendas Ok!");
 	}
 
 }
